2e800860d5e130c859074c5ac6f0beed8481e21e
jabae <shighnealex@gmail.com>
2022-09-07 23:04:28 +0900

    setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 from setuptools import setup, find_packages
 
-setup(name="Cnapse", version=0.9.0, packages=find_packages())
+with open("requirements.txt") as f:
+	required = f.read().splitlines()
+
+setup(name="Cnapse", version="0.9.0",
+			packages=find_packages(),
+			install_requires=required)
